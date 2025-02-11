5e8a5bc6c663eb5ce57b6ed3ad61ab67d7f0a605
jeon125 <eugine2001@gmail.com>
2025-02-11 14:24:30 +0900

    Add files via upload

--- app7.py
+++ app7.py
@@ -36,7 +36,7 @@ def load_model(model_path, num_classes, device):
     model = fasterrcnn_resnet50_fpn(pretrained=True)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.load_state_dict(torch.load(model_path, map_location=device), strict=False)
     model.to(device)
     model.eval()
     return model
